c701f03044b79da5c7e1a69cf585615c9aed9c77
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-05-29 02:06:57 +0300

    fix default ordering

--- src/main/java/mouse/project/lib/ioc/Inj.java
+++ src/main/java/mouse/project/lib/ioc/Inj.java
@@ -8,6 +8,6 @@ public interface Inj {
     <T> T get(Class<T> clazz);
     <T> T get(Class<T> clazz, String name);
     <T> Collection<T> getAll(Class<T> clazz);
-    Collection<Object> getFiltered(Collection<ImplementationFilter> filters);
     <T> Collection<T> getAll(Class<T> clazz, String name);
+    Collection<Object> getFiltered(Collection<ImplementationFilter> filters);
 }
